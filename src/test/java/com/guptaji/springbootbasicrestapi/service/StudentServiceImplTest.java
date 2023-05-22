5689a5420344ff1d463ed46170a0befe6f583a90
Ayush Gupta <ayushdgupta17@gmail.com>
2023-05-22 11:28:33 +0530

    complete the testing of service layer with custom, native query and custom finder methods

--- src/test/java/com/guptaji/springbootbasicrestapi/service/StudentServiceImplTest.java
+++ src/test/java/com/guptaji/springbootbasicrestapi/service/StudentServiceImplTest.java
@@ -92,4 +92,71 @@ class StudentServiceImplTest {
 
     assertTrue(result);
   }
+
+  @Test
+  void fetchAllStudentUsingJPQL() {
+
+    given(studentRepo.getAllStudentsUsingJPQL()).willReturn(studentList);
+
+    List<Student> studentList1 = studentService.fetchAllStudentUsingJPQL();
+
+    assertEquals(studentList.size(), studentList1.size());
+  }
+
+  @Test
+  void fetchAllStudentUsingFirstAndLastName() {
+
+    given(studentRepo.getAllStudentsUsingFirstAndLastName("Naruto", "Uzumaki"))
+        .willReturn(studentList);
+
+    List<Student> studentList1 =
+        studentService.fetchAllStudentUsingFirstAndLastName("Naruto", "Uzumaki");
+
+    assertEquals(studentList.size(), studentList1.size());
+  }
+
+  @Test
+  void fetchAllStudentUsingNative() {
+    given(studentRepo.getAllStudentsUsingNative()).willReturn(studentList);
+
+    List<Student> studentList1 = studentService.fetchAllStudentUsingNative();
+
+    assertEquals(studentList.size(), studentList1.size());
+  }
+
+  @Test
+  void updateStudentLastName() {
+    willDoNothing().given(studentRepo).updateLastNameUsingFirstByJPQL("Naruto", "Namikaze");
+    given(studentRepo.getAllStudentsUsingJPQL())
+        .willReturn(
+            List.of(
+                new Student(
+                    student.getFirstName(),
+                    "Namikaze",
+                    student.getClassName(),
+                    student.getRollNo())));
+
+    List<Student> studentList1 = studentService.updateStudentLastName("Naruto", "Namikaze");
+
+    assertEquals(1, studentList1.size());
+    assertEquals("Namikaze", studentList1.get(0).getLastName());
+  }
+
+  @Test
+  void findStudentsByFirstName() {
+    given(studentRepo.findByFirstName("Naruto")).willReturn(studentList);
+
+    List<Student> studentList1 = studentService.findStudentsByFirstName("Naruto");
+
+    assertEquals(studentList.size(), studentList1.size());
+  }
+
+  @Test
+  void findStudentsByFirstAndLastName() {
+    given(studentRepo.findByFirstNameOrLastName("Naruto", "Uzumaki")).willReturn(studentList);
+
+    List<Student> studentList1 = studentService.findStudentsByFirstAndLastName("Naruto", "Uzumaki");
+
+    assertEquals(studentList.size(), studentList1.size());
+  }
 }
